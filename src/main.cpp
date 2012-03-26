8f0c7582580a4d8f53b1d13e2d93c07824b7bf8f
gamemaster101gr <gamemaster@101gr.com>
2012-03-26 11:57:24 +0400

    offline/online versions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <IwGx.h>
 #include "s3eKeyboard.h"
+#include "s3eOSExec.h"
 #include "s3ePointer.h"
 #include "s3eWebView.h"
 
@@ -17,7 +18,21 @@ int32 WebViewFailedLoading(s3eWebView *instance, void *systemData, void *userDat
 
 int32 WebViewUrlLoading(s3eWebView *instance, void *systemData, void *userData)  
 {
-	//const char* url = (const char*)systemData;
+	const char* url = (const char*)systemData;
+	
+	const char* touchlink = "s3e://jqtouch";
+	if (strncmp(url, touchlink, strlen(touchlink)) == 0)
+	{
+		if (s3eOSExecAvailable())
+			s3eOSExecExecute("http://www.jqtouch.com/preview/demos/main/", true);
+	}
+	const char* mobilelink = "s3e://jqmobile";
+	if (strncmp(url, mobilelink, strlen(mobilelink)) == 0)
+	{
+		if (s3eOSExecAvailable())
+			s3eOSExecExecute("http://jquerymobile.com/demos/1.1.0-rc.1/", true);
+	}
+	
 	return 0;
 }
 
@@ -51,8 +66,9 @@ int main (int argc, char* argv[])
 	webView = s3eWebViewCreate();
 	
 	/*s3eWebViewRegister(S3E_WEBVIEW_FAILED_LOADING,WebViewFailedLoading,0,webView);
+	*/
 	s3eWebViewRegister(S3E_WEBVIEW_CUSTOM_URL_LOADING,WebViewUrlLoading,0,webView);
-	s3eWebViewRegister(S3E_WEBVIEW_FROM_JAVASCRIPT,WebViewFromJavascript,0,webView);*/
+	s3eWebViewRegister(S3E_WEBVIEW_FROM_JAVASCRIPT,WebViewFromJavascript,0,webView);
 
 	IwGxRegister(IW_GX_SCREENSIZE, rotationCallback);
 	s3eWebViewShow(webView, 0, 0, IwGxGetScreenWidth(), IwGxGetScreenHeight());
@@ -83,8 +99,9 @@ int main (int argc, char* argv[])
 		IwGxSwapBuffers();
 	}
 	/*s3eWebViewUnRegister(S3E_WEBVIEW_FAILED_LOADING,WebViewFailedLoading,webView);
+	*/
 	s3eWebViewUnRegister(S3E_WEBVIEW_CUSTOM_URL_LOADING,WebViewUrlLoading,webView);
-	s3eWebViewUnRegister(S3E_WEBVIEW_FROM_JAVASCRIPT,WebViewFromJavascript,webView);*/
+	s3eWebViewUnRegister(S3E_WEBVIEW_FROM_JAVASCRIPT,WebViewFromJavascript,webView);
 	s3eWebViewDestroy(webView);
 
 	//IwGraphicsTerminate();
